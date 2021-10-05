1f839d4ff6e2d832f476c6bc624006bee3f21afe
Yash-271120 <yashpatil.yp2711@gmail.com>
2021-10-05 12:46:20 +0530

    My Contribution

---
+++ contributions/Yash-271120/myhacktoberfest.cpp
@@ -0,0 +1,15 @@
+// NAME: Yash Patil
+// GITHUB USERNAMME: Yash-271120
+// COUNTRY: India
+// PROGRAMMING LANGUAGE: C++
+// I LOVE HACKTOBERFEST2021
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Hacktoberfest2021!";
+    return 0;
+}
